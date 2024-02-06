9ed803bd4e0572018d1bcd842d29d6a3669342a3
GaurabCodeLab <kumargaurab56@gmail.com>
2024-02-07 00:57:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App(){
   return (
     <>
     <Routes>
-      <Route path="/" element={<Create/>} />
+      <Route path="/CRUD_App_React" element={<Create/>} />
       <Route path="/read" element={<Read/>} />
       <Route path="/update/:id" element={<Update/>} />
       <Route path="*" element={<h1 className='text-center mt-4'>Page Not Found</h1>} />
@@ -19,77 +19,3 @@ function App(){
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import './App.css';
-// import './style.css'
-// import aar from './custom.module.css';
-// import React from 'react';
-// function App() {
-//   const styleItem={color:'red',backgroundColor:'black'}
-//   console.log(aar, "aar hai");
-//   return (
-//     <div className="App">
-//       <h1 className="primary">Style type 1 in React js</h1>
-//       <h1 style={styleItem}>Style type 2 in React js</h1>
-//       <h1 id={aar.laura} >Style type 3 in React js</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
